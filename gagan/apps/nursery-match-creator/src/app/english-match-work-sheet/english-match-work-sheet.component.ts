1c6a13b079e868b270f298ae0668f8320006f9aa
Singh <GAGDS@barco.com>
2020-02-09 23:11:32 +0530

    match sheet created.

--- gagan/apps/nursery-match-creator/src/app/english-match-work-sheet/english-match-work-sheet.component.ts
+++ gagan/apps/nursery-match-creator/src/app/english-match-work-sheet/english-match-work-sheet.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Route } from '@angular/compiler/src/core';
 
+interface item {
+  left: string,
+  right: string
+}
+
 @Component({
   selector: 'gagan-english-match-work-sheet',
   templateUrl: './english-match-work-sheet.component.html',
@@ -9,13 +14,19 @@ import { Route } from '@angular/compiler/src/core';
 })
 export class EnglishMatchWorkSheetComponent implements OnInit {
 
+  public items: item[] = [];
   constructor(private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
     this.route.queryParamMap.subscribe(params => {
-      console.log(params);
+      for (let i = 0; i < 24; i++) {
+        this.items.push({
+          left: "a",
+          right: "b"
+        });
+      }
     });
   }
 
